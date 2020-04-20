07c56f85e822a938c492ca00da812cf0cb241e9b
FujiApple <fujiapple852@gmail.com>
2020-04-20 16:55:31 +0800

    Darn

--- sample_apps/darn/src/command_download.rs
+++ sample_apps/darn/src/command_download.rs
@@ -25,17 +25,17 @@ pub async fn download(uri: &str, token_key_path: Option<&str>, main_package_name
         .find(|p| p.package_name == main_package_name)
         .ok_or_else(|| DarnError::unknown_package(main_package_name))?;
 
-    let included_package: HashMap<&String, &String> =
-        once((&main_package_info.package_id, &main_package_info.package_name))
-            .chain(dependencies.iter().map(|d| (&d.package_id, &d.package_name)))
+    let included_package: HashMap<&str, &str> =
+        once((main_package_info.package_id.as_str(), main_package_info.package_name.as_str()))
+            .chain(dependencies.iter().map(|d| (d.package_id.as_str(), d.package_name.as_str())))
             .collect();
 
     // 3: we have to filter and rename each DamlLfArchive with the name we have just extracted from the package
-    let renamed: HashMap<&String, DamlLfArchive> = once(working_dar.main)
+    let renamed: HashMap<&str, DamlLfArchive> = once(working_dar.main)
         .chain(working_dar.dependencies)
         .filter_map(|mut a| {
-            included_package.get_key_value(&a.hash).map(|(&k, &v)| {
-                a.name = v.clone();
+            included_package.get_key_value(a.hash.as_str()).map(|(&k, &v)| {
+                a.name = v.to_string();
                 (k, a)
             })
         })
@@ -101,7 +101,7 @@ fn make_working_dar(mut all_packages: Vec<DamlLfArchive>) -> DarFile {
     assemble_dar(first, rest)
 }
 
-fn make_final_dar(mut all_packages: HashMap<&String, DamlLfArchive>, package_id: &String) -> DarFile {
+fn make_final_dar(mut all_packages: HashMap<&str, DamlLfArchive>, package_id: &str) -> DarFile {
     let main_package = all_packages.remove(package_id).unwrap();
     assemble_dar(main_package, all_packages.into_iter().map(|(_, v)| v).collect())
 }
