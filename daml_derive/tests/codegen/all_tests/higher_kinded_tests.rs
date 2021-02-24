b6da48e8aa11b16a75f03cdfcdcdb524b9a73502
FujiApple <fujiapple852@gmail.com>
2021-02-24 17:26:51 +0800

    DAML SDK 1.11.1

--- daml_derive/tests/codegen/all_tests/higher_kinded_tests.rs
+++ daml_derive/tests/codegen/all_tests/higher_kinded_tests.rs
@@ -4,7 +4,7 @@ use daml::grpc_api::serialize::{DamlDeserializeInto, DamlSerializeFrom};
 use daml_derive::daml_codegen;
 
 daml_codegen!(
-    dar_file = r"resources/testing_types_sandbox/archive/TestingTypes-1_0_0-sdk_1_10_0-lf_1_11.dar",
+    dar_file = r"resources/testing_types_sandbox/archive/TestingTypes-1_0_0-sdk_1_11_0-lf_1_11.dar",
     module_filter_regex = "DA.HigherKindTest"
 );
 
