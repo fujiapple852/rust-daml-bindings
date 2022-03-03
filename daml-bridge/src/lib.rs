ebb853071e210bf000f2ac52f136e91eaa3cb9fa
FujiApple <fujiapple852@gmail.com>
2022-03-03 14:35:13 +0800

    doc: added `html_root_url` attribute to every crate

--- daml-bridge/src/lib.rs
+++ daml-bridge/src/lib.rs
@@ -13,6 +13,7 @@
 #![forbid(unsafe_code)]
 #![doc(html_favicon_url = "https://docs.daml.com/_static/images/favicon/favicon-32x32.png")]
 #![doc(html_logo_url = "https://docs.daml.com/_static/images/DAML_Logo_Blue.svg")]
+#![doc(html_root_url = "https://docs.rs/daml-bridge/0.1.0")]
 
 mod aliases;
 mod bridge;
